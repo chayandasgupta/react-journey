d820de04acb29dc74ad91ab8ad53bdee27648ccd
chayandasgupta <chayandas285178@gmail.com>
2025-07-13 23:58:24 +0600

    feat: add route layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes
-} from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
-
-import About from "./pages/About";
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import PostDetail from "./pages/PostDetail";
-import Posts from "./pages/Posts";
+
+const About = lazy(() => new Promise(resolve => setTimeout(() => resolve(import("./pages/About")), 2000)));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const PostDetail = lazy(() => import("./pages/PostDetail"));
+const Posts = lazy(() => import("./pages/dashboard/Posts"));
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/post/:postId" element={<PostDetail />} />
-        <Route path="/login" element={<Login />} />
-        
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="posts" element={<Posts />} />
-        </Route>
+      <Suspense fallback={<div>Locading....</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/post/:postId" element={<PostDetail />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="posts" element={<Posts />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
